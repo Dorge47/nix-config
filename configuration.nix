e543d3d59e389e317cbbb582721cd97acf49e33f
Dorge47 <Dorge47@users.noreply.github.com>
2024-04-17 19:47:47 -0700

    Fix bootloader

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Enable SSH on boot
+  boot.kernelParams = [ "ip=dhcp" ];
   boot.initrd.network.enable = true;
   boot.initrd.network.ssh = {
     enable = true;
@@ -22,6 +23,9 @@
     authorizedKeys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCg88GUiVCOaDnv2IRdcLDrnpTvpgFRi9Dzyf8l78ikoRX2vEkmT2ucUXvMhFULL9LHw1qDHgGctTCT2cr3VSJ17r/8Fbafq18Y7D6L1n9wgK4khdpkqy7BYHowbPpygroDAVGHIu5wgMaFGOHcanQntcFBqhGAhLYri0XQSO2OHlEgQFmVBS/usBAfcRMmxTzQ9QKFf/NLixymTQcXOO1grIqfBL6Y1vtXihPbU0xrmH6uNnlJqK/xVMqu3w5g09sLUZFoJjB2hQTaLyfAkiU4Co4HSktNzPqCc8a0TBxW32WjCcu6jinvPdPzBrQdR1+N4235lyA1vR27srxkd0gSgl84Dl2DXhfdbRUfg6sjQvvnNlxGHBXLZFmh8qGO8WOlppT9+3KSDYx/LVubRm8M7opyy3Q8h4YsnF/Ffb+86Nm9XLHRiF+DIF4mw/7ixSqveM4jqDSfAye0FyHumoxnHFw7hA9l+XSthNPFInDUZtQw7bRFL2JY3XO6ki5AIf0= chris@Chriss-MacBook-Pro.local"
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHHcaMoJyFhLHOUCTBDFud3H0AlISWNAZzIf3m19V8sPjbJdl66/ohc0dD2tPCLu8cVTVT1Iu3Az7agRE64Ahc0="
+    ];
+    hostKeys = [
+      "/etc/secrets/initrd/ssh_host_rsa_key"
     ];
   };
 
