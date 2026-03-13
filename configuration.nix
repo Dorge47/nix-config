8dd3d6f209b9b38d6cbaec68431465a41dd27f56
Dorge47 <Dorge47@users.noreply.github.com>
2026-03-13 02:46:55 -0700

    Minegrub and syncthing

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,25 @@ in {
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    useOSProber = true;
+    device = "nodev";
+    theme = pkgs.stdenv.mkDerivation {
+      pname = "minegrub-theme";
+      version = "master";
+      src = pkgs.fetchFromGitHub {
+        owner = "Dorge47";
+        repo = "minegrub-theme";
+        rev = "master";
+        hash = "sha256-ZlKa/DLAvQ8z20Xi5qHJjuNq64EJEoYDqgjwpWUz5LA=";
+      };
+      installPhase = ''
+      mkdir -p $out
+      cp -r minegrub/* $out/'';
+    };
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Enable SSH on boot
@@ -362,6 +380,15 @@ AttrEventCode=-REL_WHEEL_HI_RES;-REL_HWHEEL_HI_RES;'';
     dumpcap.enable = true;
     usbmon.enable = true;
     package = pkgs.wireshark;
+  };
+  
+  services.syncthing = {
+    enable = true;
+    relay.enable = false;
+    openDefaultPorts = true;
+    user = "chris";
+    dataDir = "/home/chris";
+    configDir = "/home/chris/.config/syncthing";
   };
   
   # GnuPG
