19b72a3dd2871781b94909dd6fae5f1f41ba4db1
Dorge47 <Dorge47@users.noreply.github.com>
2026-07-03 05:29:27 -0700

    Fix permissions for fileserver mount

--- modules/services/desktop-mounts.nix
+++ modules/services/desktop-mounts.nix
@@ -13,7 +13,17 @@
   fileSystems."/run/media/chris/fileserver" = #Unraid server
     { device = secrets.unraidPath;
       fsType = "cifs";
-      options = [ "credentials=/home/chris/Documents/GitHub/nix-config/secrets/desktop-credentials.txt" ];
+      options = [
+        "credentials=/home/chris/Documents/GitHub/nix-config/secrets/desktop-credentials.txt"
+        "uid=1000"
+        "gid=100"
+        "file_mode=0664"
+        "dir_mode=0775"
+        "rw"
+        "vers=3.1.1"
+        "nofail"
+        "x-systemd.automount"
+      ];
     };
   
   fileSystems."/".options = [ "compress=zstd:1" "noatime" ];
